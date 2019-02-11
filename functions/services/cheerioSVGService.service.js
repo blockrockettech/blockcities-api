72859b6a4981a12eaca4fcf65ddcc9282e6cfc8b
Andy Gray <andy@whatsthescore.co.uk>
2019-02-11 10:50:52 +0000

    new 4s

--- functions/services/cheerioSVGService.service.js
+++ functions/services/cheerioSVGService.service.js
@@ -2,27 +2,29 @@ const _ = require('lodash');
 
 class CheerioSVGService {
 
-    findAnchorPoint(svg) {
-        const svgId = svg.attr('id');
-        return svgId.split('_')[1];
-    }
-
-    process(svgXml, exteriorFill = {}, windowsFill = {}, concreteFill = {}) {
+    process (svgXml, exteriorFill = {}, windowsFill = {}, concreteFill = {}) {
         const $ = require('cheerio').load(svgXml, {xmlMode: true});
 
-        _.forEach(exteriorFill.left, (v, k) => $(`.exterior_x002D_L_x002D_${k}`).attr('fill', v));
-        _.forEach(exteriorFill.right, (v, k) => $(`.exterior_x002D_R_x002D_${k}`).attr('fill', v));
-        _.forEach(exteriorFill.top, (v, k) => $(`.top_x002D_${k}`).attr('fill', v));
+        _.forEach(exteriorFill.left, (v, k) => {$(`.exterior-L-${k}`).attr('fill', v)});
+        _.forEach(exteriorFill.right, (v, k) => $(`.exterior-R-${k}`).attr('fill', v));
+        _.forEach(exteriorFill.top, (v, k) => $(`.top-${k}`).attr('fill', v));
 
-        _.forEach(windowsFill.left, (v, k) => $(`.window_x002D_L_x002D_${k}`).attr('fill', v));
-        _.forEach(windowsFill.right, (v, k) => $(`.window_x002D_R_x002D_${k}`).attr('fill', v));
+        _.forEach(windowsFill.left, (v, k) => $(`.window-L-${k}`).attr('fill', v));
+        _.forEach(windowsFill.right, (v, k) => $(`.window-R-${k}`).attr('fill', v));
 
         // FIXME if darkgrey exterior then dark concrete
-        _.forEach(concreteFill.classic, (v, k) => $(`.concrete_x002D_${k}`).attr('fill', v));
+        _.forEach(concreteFill.classic, (v, k) => $(`.concrete-${k}`).attr('fill', v));
+
+        let anchorElement = $('[id^=anchor-01_]').first();
+        let anchor = undefined;
+
+        if (anchorElement && anchorElement.attr('id')) {
+            anchor = anchorElement.attr('id').split('_')[1];
+        }
 
         return {
             svg: $.xml(),
-            anchor: this.findAnchorPoint($('svg'))
+            anchor: anchor
         };
     }
 }
