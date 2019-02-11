e4ca36a9c2473a8f12729aaaca6c31910118acd9
James Morgan <james.morgan.e@googlemail.com>
2019-02-11 11:12:25 +0000

    New buildings

--- functions/services/cheerioSVGService.service.js
+++ functions/services/cheerioSVGService.service.js
@@ -3,17 +3,18 @@ const _ = require('lodash');
 class CheerioSVGService {
 
     process (svgXml, exteriorFill = {}, windowsFill = {}, concreteFill = {}) {
+
         const $ = require('cheerio').load(svgXml, {xmlMode: true});
 
-        _.forEach(exteriorFill.left, (v, k) => {$(`.exterior-L-${k}`).attr('fill', v)});
-        _.forEach(exteriorFill.right, (v, k) => $(`.exterior-R-${k}`).attr('fill', v));
-        _.forEach(exteriorFill.top, (v, k) => $(`.top-${k}`).attr('fill', v));
+        _.forEach(exteriorFill.left, (v, k) => {$(`.exterior-L-${k}`).attr('style', `fill: ${v}`)});
+        _.forEach(exteriorFill.right, (v, k) => $(`.exterior-R-${k}`).attr('style', `fill: ${v}`));
+        _.forEach(exteriorFill.top, (v, k) => $(`.top-${k}`).attr('style', `fill: ${v}`));
 
-        _.forEach(windowsFill.left, (v, k) => $(`.window-L-${k}`).attr('fill', v));
-        _.forEach(windowsFill.right, (v, k) => $(`.window-R-${k}`).attr('fill', v));
+        _.forEach(windowsFill.left, (v, k) => $(`.window-L-${k}`).attr('style', `fill: ${v}`));
+        _.forEach(windowsFill.right, (v, k) => $(`.window-R-${k}`).attr('style', `fill: ${v}`));
 
         // FIXME if darkgrey exterior then dark concrete
-        _.forEach(concreteFill.classic, (v, k) => $(`.concrete-${k}`).attr('fill', v));
+        _.forEach(concreteFill.classic, (v, k) => $(`.concrete-${k}`).attr('style', `fill: ${v}`));
 
         let anchorElement = $('[id^=anchor-01_]').first();
         let anchor = undefined;
