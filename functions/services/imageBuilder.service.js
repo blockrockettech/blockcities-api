2bc49ba16a3fd03d3f3fbed8fab3543003e9731f
Andy Gray <andy@whatsthescore.co.uk>
2019-03-19 15:56:37 +0000

    no roof 8 config

--- functions/services/imageBuilder.service.js
+++ functions/services/imageBuilder.service.js
@@ -33,6 +33,19 @@ class ImageBuilderService {
         }) {
 
         try {
+
+            if (building === 8) {
+                return await this.generateNoRoofImage(
+                    {
+                        building,
+                        base,
+                        body,
+                        exteriorColorway,
+                        windowColorway,
+                    }
+                );
+            }
+
             const rootPath = `${__dirname}/../raw_svgs/${building}`;
 
             const basePath = `${rootPath}/Bases/${base}.svg`;
@@ -157,6 +170,111 @@ class ImageBuilderService {
                 0,
                 adjustedBodyWidthPath,
                 adjustedRoofHeight
+            );
+
+            return canvas.toBuffer('image/svg+xml', {
+                title: `BlockCities`,
+                keywords: 'BlockCities',
+                creationDate: new Date()
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async generateNoRoofImage (
+        {
+            building,
+            base,
+            body,
+            exteriorColorway,
+            windowColorway
+        }) {
+
+        try {
+            const rootPath = `${__dirname}/../raw_svgs/${building}`;
+
+            const basePath = `${rootPath}/Bases/${base}.svg`;
+            const bodyPath = `${rootPath}/Bodies/${body}.svg`;
+
+            const rawBaseSvg = await readFilePromise(basePath, 'utf8');
+            const rawBodySvg = await readFilePromise(bodyPath, 'utf8');
+
+            const {
+                svg: processedBaseSvg,
+                anchorX: processedBaseAnchorX,
+                anchorY: processedBaseAnchorY,
+                anchorWidthPath: processedBaseAnchorWidthPath
+            } = cheerioSVGService.process(
+                rawBaseSvg,
+                colourways.exteriors[exteriorsKeys[exteriorColorway]],
+                colourways.windows[windowsKeys[windowColorway]],
+                colourways.curtains[curtainsKeys[windowColorway]],
+                colourways.concrete,
+            );
+            const {
+                svg: processedBodySvg,
+                anchorX: processedBodyAnchorX,
+                anchorY: processedBodyAnchorY,
+                anchorWidthPath: processedBodyAnchorWidthPath
+            } = cheerioSVGService.process(
+                rawBodySvg,
+                colourways.exteriors[exteriorsKeys[exteriorColorway]],
+                colourways.windows[windowsKeys[windowColorway]],
+                colourways.curtains[curtainsKeys[windowColorway]],
+                colourways.concrete,
+            );
+
+            const baseImage = await loadImage(Buffer.from(processedBaseSvg, 'utf8'));
+            const bodyImage = await loadImage(Buffer.from(processedBodySvg, 'utf8'));
+
+            const baseConfig = {
+                width: baseImage.width,
+                height: baseImage.height,
+                anchorX: parseFloat(processedBaseAnchorX),
+                anchorY: parseFloat(processedBaseAnchorY),
+                anchorWidthPath: parseFloat(processedBaseAnchorWidthPath),
+                svg: baseImage
+            };
+            const bodyConfig = {
+                width: bodyImage.width,
+                height: bodyImage.height,
+                svg: bodyImage
+            };
+
+            const adjustedBodyHeight = bodyConfig.height * (baseConfig.anchorWidthPath / bodyConfig.width);
+
+            // height of the baseConfig, bodyConfig, roofConfig - minus the difference in the offset anchor from bodyConfig and height
+            const canvasHeight = baseConfig.height
+                + adjustedBodyHeight
+                - baseConfig.anchorY;
+
+            // Always assume the baseConfig if the widest part for now
+            const canvasWidth = baseConfig.width;
+
+            const canvas = createCanvas(canvasWidth, canvasHeight, 'svg');
+            const ctx = canvas.getContext('2d');
+
+            console.log(`base config`, baseConfig);
+            console.log(`body config`, bodyConfig);
+
+            const startBaseY = canvasHeight - baseConfig.height;
+            const startBodyY = canvasHeight - adjustedBodyHeight;
+
+            // Base
+            ctx.drawImage(
+                baseConfig.svg,
+                0,
+                startBaseY
+            );
+
+            // Body
+            ctx.drawImage(
+                bodyConfig.svg,
+                baseConfig.anchorX,
+                startBodyY - baseConfig.height + baseConfig.anchorY,
+                baseConfig.anchorWidthPath,
+                adjustedBodyHeight,
             );
 
             return canvas.toBuffer('image/svg+xml', {
