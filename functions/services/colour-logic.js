d3111fa50709b08f16da948ab5b70b1a9fc2103e
Andy Gray <andy@whatsthescore.co.uk>
2019-03-29 20:39:13 +0000

    retail bases

---
+++ functions/services/colour-logic.js
@@ -0,0 +1,47 @@
+// Glass Curtain Buildings
+// | 0  | Red         | Dark Grey | Dark Grey | Dark Grey |
+// | -- | ----------- | --------- | --------- | --------- |
+// |    |             |           |           |           |
+// | 1  | Dark Grey   | Gold      | Gold      | Gold      |
+// | 2  | Dark Grey   | Aqua Blue | Aqua Blue | Aqua Blue |
+// | 3  | Dark Grey   | True Blue | True Blue | True Blue |
+// |    |             |           |           |           |
+// | 4  | Light Beige | Gold      | Gold      | Gold      |
+// | 5  | Light Beige | Aqua Blue | Aqua Blue | Aqua Blue |
+// | 6  | Light Beige | True Blue | True Blue | True Blue |
+// | 7  | Light Beige | Dark Grey | Dark Grey | Dark Grey |
+// | 8  | Light Beige | Dark Grey | Dark Grey | Gold      |
+// |    |             |           |           |           |
+// | 9  | Dark Brown  | Gold      | Gold      | Gold      |
+// |    |             |           |           |           |
+// | 10 | Light Grey  | Gold      | Gold      | Gold      |
+// | 11 | Light Grey  | Dark Grey | Dark Grey | Dark Grey |
+// | 12 | Light Grey  | Dark Grey | Dark Grey | Gold      |
+// | 13 | Light Grey  | Gold      | Gold      | Dark Grey |
+// |    |             |           |           |           |
+// | 14 | Black       | Gold      | Gold      | Gold      |
+// | 15 | Black       | Dark Grey | Dark Grey | Dark Grey |
+// | 16 | Black       | Dark Grey | Dark Grey | Gold      |
+// | 17 | Black       | Gold      | Gold      | Dark Grey |
+// | 18 | Black       | Aqua Blue | Aqua Blue | Aqua Blue |
+// | 19 | Black       | True Blue | True Blue | True Blue |
+// | 20 | TODO        | TODO      | TODO      | TODO      |
+
+
+
+
+module.exports = {
+    curtains: [
+        ['red', 'darkgrey', 'darkgrey', 'darkgrey'], // 0
+        ['darkgrey', 'gold', 'gold', 'gold'], // 1
+        ['darkgrey', 'aquablue', 'aquablue', 'aquablue'], // 2
+        ['darkgrey', 'aquablue', 'aquablue', 'aquablue'], // 3
+        ['darkgrey', 'aquablue', 'aquablue', 'aquablue'], // 4
+        ['darkgrey', 'aquablue', 'aquablue', 'aquablue'], // 5
+        ['darkgrey', 'aquablue', 'aquablue', 'aquablue'], // 6
+        ['darkgrey', 'aquablue', 'aquablue', 'aquablue'], // 7
+        ['darkgrey', 'aquablue', 'aquablue', 'aquablue'], // 8
+        ['darkgrey', 'aquablue', 'aquablue', 'aquablue'], // 9
+        ['darkgrey', 'aquablue', 'aquablue', 'aquablue'], // 10
+    ]
+};
