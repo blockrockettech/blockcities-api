8aec4ec04f0992c08ca2867e5ecdee34840264a1
James Morgan <james.morgan.e@googlemail.com>
2019-04-02 13:13:07 +0100

    Adding special mappers

--- functions/tokenUri/special-data-mapping.js
+++ functions/tokenUri/special-data-mapping.js
@@ -11,6 +11,11 @@ module.exports = {
     8: {name: 'One California', bname: 'US Bank', city: ' San Francisco'},
     9: {name: 'One Bryant Park', bname: 'BoA Tower', city: 'New York City'},
     10: {name: '56 Leonard', bname: '56 Leonard', city: 'New York City'},
-    11: {name: '601 Lexington', bname: 'Citigroup Center  ', city: 'New York City'},
-};
+    11: {name: '601 Lexington', bname: 'Citigroup Center', city: 'New York City'},
 
+    1000000: {name: 'BlockCities Genesis Building', bname: 'Genesis', city: 'Genesis'},
+    1000001: {name: '375 Park Ave', bname: '375 Park Ave', city: 'New York City'},
+    1000002: {name: '133 Peachtree', bname: '133 Peachtree', city: 'Atlanta'},
+    1000003: {name: '200 East Randolph', bname: '200 East Randolph', city: 'Chicago'},
+    1000004: {name: '600 Montgomery', bname: '600 Montgomery', city: 'San Francisco'}
+};
