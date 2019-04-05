ecadc584bd24f1678f8d18f6b89a76bc65345d80
James Morgan <james.morgan.e@googlemail.com>
2019-04-05 16:37:19 +0100

    Adding refresh script

--- scripts/opensea/refresh-metadata.js
+++ scripts/opensea/refresh-metadata.js
@@ -14,8 +14,8 @@ void async function () {
 
     const NETWORK = 1;
 
-    const {tokenIdPointer} = await blockcitiesContractService.tokenPointers(4);
-    console.log('tokenIdPointer', tokenIdPointer);
+    const {tokenIdPointer} = await blockcitiesContractService.tokenPointers(NETWORK);
+    console.log(`Total tokens ${tokenIdPointer}`);
 
     for (let i = 1; i < tokenIdPointer; i++) {
         await openSeaService.refreshTokenMetaData(NETWORK, i);
