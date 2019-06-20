d75a022433b1e9c9b1fac7a80c575f07df84a9b5
Andy Gray <andy@whatsthescore.co.uk>
2019-06-20 10:15:36 +0100

    CMS work

--- functions/services/imageBuilder.service.js
+++ functions/services/imageBuilder.service.js
@@ -13,7 +13,7 @@ const xPadding = 0;
 
 class ImageBuilderService {
 
-    async loadSpecial (specialId) {
+    async loadSpecial(specialId) {
 
         try {
             const path = `${__dirname}/../raw_svgs/specials/${specialId}.svg`;
@@ -24,7 +24,7 @@ class ImageBuilderService {
         }
     }
 
-    async generateImage (
+    async generateImage(
         {
             building,
             base,
@@ -208,7 +208,7 @@ class ImageBuilderService {
     }
 
     // FIXME - extract common logic
-    async generateNoRoofImage (
+    async generateNoRoofImage(
         {
             building,
             base,
@@ -308,6 +308,224 @@ class ImageBuilderService {
                 keywords: 'BlockCities',
                 creationDate: new Date()
             });
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    // FIXME when and if we know this works then rework into one shared function
+    async generateImageStats({
+            building,
+            base,
+            body,
+            roof,
+            exteriorColorway,
+            backgroundColorway,
+        }) {
+
+        try {
+            if (parseInt(building) === 8) {
+                return await this.generateNoRoofImageStats(
+                    {
+                        building,
+                        base,
+                        body,
+                        exteriorColorway,
+                        backgroundColorway,
+                    }
+                );
+            }
+
+            const rootPath = `${__dirname}/../raw_svgs/${building}`;
+
+            const basePath = `${rootPath}/Bases/${base}.svg`;
+            const bodyPath = `${rootPath}/Bodies/${body}.svg`;
+            const roofPath = `${rootPath}/Roofs/${roof}.svg`;
+
+            const rawBaseSvg = await readFilePromise(basePath, 'utf8');
+            const rawBodySvg = await readFilePromise(bodyPath, 'utf8');
+            const rawRoofSvg = await readFilePromise(roofPath, 'utf8');
+
+            const {
+                svg: processedBaseSvg,
+                anchorX: processedBaseAnchorX,
+                anchorY: processedBaseAnchorY,
+                anchorWidthPath: processedBaseAnchorWidthPath
+            } = cheerioSVGService.process(
+                rawBaseSvg,
+                colourways.exteriors[colourLogic[exteriorColorway][0]],
+                colourways.windows[colourLogic[exteriorColorway][3]],
+                colourways.curtains[colourLogic[exteriorColorway][3]],
+            );
+
+            const {
+                svg: processedBodySvg,
+                anchorX: processedBodyAnchorX,
+                anchorY: processedBodyAnchorY,
+                anchorWidthPath: processedBodyAnchorWidthPath
+            } = cheerioSVGService.process(
+                rawBodySvg,
+                colourways.exteriors[colourLogic[exteriorColorway][0]],
+                colourways.windows[colourLogic[exteriorColorway][2]],
+                colourways.curtains[colourLogic[exteriorColorway][2]],
+            );
+
+            const {svg: processedRoofSvg} = cheerioSVGService.process(
+                rawRoofSvg,
+                colourways.exteriors[colourLogic[exteriorColorway][0]],
+                colourways.windows[colourLogic[exteriorColorway][1]],
+                colourways.curtains[colourLogic[exteriorColorway][1]],
+            );
+
+            const baseImage = await loadImage(Buffer.from(processedBaseSvg, 'utf8'));
+            const bodyImage = await loadImage(Buffer.from(processedBodySvg, 'utf8'));
+            const roofImage = await loadImage(Buffer.from(processedRoofSvg, 'utf8'));
+
+            const baseConfig = {
+                width: baseImage.width,
+                height: baseImage.height,
+                anchorX: parseFloat(processedBaseAnchorX),
+                anchorY: parseFloat(processedBaseAnchorY),
+                anchorWidthPath: parseFloat(processedBaseAnchorWidthPath),
+                svg: baseImage
+            };
+            const bodyConfig = {
+                width: bodyImage.width,
+                height: bodyImage.height,
+                anchorX: parseFloat(processedBodyAnchorX),
+                anchorY: parseFloat(processedBodyAnchorY),
+                anchorWidthPath: parseFloat(processedBodyAnchorWidthPath),
+                svg: bodyImage
+            };
+            const roofConfig = {
+                width: roofImage.width,
+                height: roofImage.height,
+                svg: roofImage
+            };
+
+            // check and log for dodgy anchors
+            if (
+                isNaN(baseConfig.anchorWidthPath) ||
+                isNaN(bodyConfig.anchorWidthPath) ||
+                isNaN(baseConfig.anchorX) ||
+                isNaN(baseConfig.anchorY) ||
+                isNaN(bodyConfig.anchorX) ||
+                isNaN(bodyConfig.anchorY)
+            ) {
+                console.error(`NaN detected: Building ${building} Base ${base} Body ${body} Roof ${roof}`);
+            }
+
+            const adjustedBodyHeight = bodyConfig.height * (baseConfig.anchorWidthPath / bodyConfig.width);
+            const adjustedBodyAnchorY = bodyConfig.anchorY * (adjustedBodyHeight / bodyConfig.height);
+
+            const adjustedBodyWidthPath = bodyConfig.anchorWidthPath * (baseConfig.anchorWidthPath / bodyConfig.width);
+            const adjustedBodyWidth = bodyConfig.width * (baseConfig.anchorWidthPath / bodyConfig.width);
+            const adjustedBodyAnchorX = bodyConfig.anchorX * (baseConfig.anchorWidthPath / bodyConfig.width);
+
+            // fixes 12 - is this the solution to scale roofs?
+            const adjustedRoofHeight = roofConfig.height * (adjustedBodyWidthPath / roofConfig.width);
+
+            let canvasHeight = baseConfig.height
+                + adjustedBodyHeight
+                + adjustedRoofHeight
+                - baseConfig.anchorY
+                - adjustedBodyAnchorY
+                + (yPadding * 2);
+
+            // roof nudge is used if the roof does not overlap the top of the body fully
+            let roofNudge = 0;
+            if (adjustedBodyAnchorY > adjustedRoofHeight) {
+                roofNudge = adjustedBodyAnchorY - adjustedRoofHeight;
+                canvasHeight = canvasHeight + roofNudge;
+            }
+
+            return {
+                adjustedBodyHeight,
+                adjustedBodyWidth,
+                canvasHeight,
+            };
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    // FIXME when and if we know this works then rework into one shared function
+    async generateNoRoofImageStats(
+        {
+            building,
+            base,
+            body,
+            roof,
+            exteriorColorway,
+            backgroundColorway,
+        }) {
+
+        try {
+            const rootPath = `${__dirname}/../raw_svgs/${building}`;
+
+            const basePath = `${rootPath}/Bases/${base}.svg`;
+            const bodyPath = `${rootPath}/Bodies/${body}.svg`;
+
+            const rawBaseSvg = await readFilePromise(basePath, 'utf8');
+            const rawBodySvg = await readFilePromise(bodyPath, 'utf8');
+
+            const {
+                svg: processedBaseSvg,
+                anchorX: processedBaseAnchorX,
+                anchorY: processedBaseAnchorY,
+                anchorWidthPath: processedBaseAnchorWidthPath
+            } = cheerioSVGService.process(
+                rawBaseSvg,
+                colourways.exteriors[colourLogic[exteriorColorway][0]],
+                colourways.windows[colourLogic[exteriorColorway][3]],
+                colourways.curtains[colourLogic[exteriorColorway][3]],
+            );
+
+            const {
+                svg: processedBodySvg,
+                anchorX: processedBodyAnchorX,
+                anchorY: processedBodyAnchorY,
+                anchorWidthPath: processedBodyAnchorWidthPath
+            } = cheerioSVGService.process(
+                rawBodySvg,
+                colourways.exteriors[colourLogic[exteriorColorway][0]],
+                colourways.windows[colourLogic[exteriorColorway][2]],
+                colourways.curtains[colourLogic[exteriorColorway][2]],
+            );
+
+            const baseImage = await loadImage(Buffer.from(processedBaseSvg, 'utf8'));
+            const bodyImage = await loadImage(Buffer.from(processedBodySvg, 'utf8'));
+
+            const baseConfig = {
+                width: baseImage.width,
+                height: baseImage.height,
+                anchorX: parseFloat(processedBaseAnchorX),
+                anchorY: parseFloat(processedBaseAnchorY),
+                anchorWidthPath: parseFloat(processedBaseAnchorWidthPath),
+                svg: baseImage
+            };
+            const bodyConfig = {
+                width: bodyImage.width,
+                height: bodyImage.height,
+                svg: bodyImage
+            };
+
+            const adjustedBodyHeight = bodyConfig.height * (baseConfig.anchorWidthPath / bodyConfig.width);
+
+            // height of the baseConfig, bodyConfig, roofConfig - minus the difference in the offset anchor from bodyConfig and height
+            const canvasHeight = baseConfig.height
+                + adjustedBodyHeight
+                - baseConfig.anchorY
+                + (yPadding * 2);
+
+
+            return {
+                adjustedBodyHeight,
+                adjustedBodyWidth: bodyConfig.width,
+                canvasHeight,
+            };
+
         } catch (e) {
             console.error(e);
         }
--- functions/tokenUri/special-data-mapping.js
+++ functions/tokenUri/special-data-mapping.js
@@ -1,4 +1,12 @@
-const {config: cityConfig} = require('./citymapper');
+const {
+    Atlanta,
+    NYC,
+    Chicago,
+    SanFrancisco,
+    Tokyo,
+    London
+} = require('./cities');
+
 const {config: backgrounConfig} = require('../services/background-colours');
 
 // Don't export bname to the public!
@@ -6,82 +14,82 @@ module.exports = {
     0: {
         name: '241 Ralph McGill',
         bname: 'Georgia Power',
-        city: cityConfig.Atlanta.name,
-        cityId: cityConfig.Atlanta.id,
+        city: Atlanta.name,
+        cityId: Atlanta.id,
     },
     1: {
         name: '133 Peachtree',
         bname: 'GP Tower',
-        city: cityConfig.Atlanta.name,
-        cityId: cityConfig.Atlanta.id,
+        city: Atlanta.name,
+        cityId: Atlanta.id,
     },
     2: {
         name: '210 Peachtree',
         bname: 'Westin Peachtree Plaza',
-        city: cityConfig.Atlanta.name,
-        cityId: cityConfig.Atlanta.id,
+        city: Atlanta.name,
+        cityId: Atlanta.id,
     },
     3: {
         name: '150 North Michigan',
         bname: 'Crain Communications',
-        city: cityConfig.Chicago.name,
-        cityId: cityConfig.Chicago.id,
+        city: Chicago.name,
+        cityId: Chicago.id,
     },
     4: {
         name: '401 North Wabash',
         bname: 'Trump Tower Chicago',
-        city: cityConfig.Chicago.name,
-        cityId: cityConfig.Chicago.id,
+        city: Chicago.name,
+        cityId: Chicago.id,
     },
     5: {
         name: '233 South Wacker',
         bname: 'Willis Tower',
-        city: cityConfig.Chicago.name,
-        cityId: cityConfig.Chicago.id,
+        city: Chicago.name,
+        cityId: Chicago.id,
     },
     6: {
         name: '222 Second Street',
         bname: 'Linkedin Building',
-        city: cityConfig.SanFrancisco.name,
-        cityId: cityConfig.SanFrancisco.id,
+        city: SanFrancisco.name,
+        cityId: SanFrancisco.id,
     },
     7: {
         name: '600 Montgomery',
         bname: 'Transamerica Pyramid',
-        city: cityConfig.SanFrancisco.name,
-        cityId: cityConfig.SanFrancisco.id,
+        city: SanFrancisco.name,
+        cityId: SanFrancisco.id,
     },
     8: {
         name: 'One California',
         bname: 'US Bank',
-        city: cityConfig.SanFrancisco.name,
-        cityId: cityConfig.SanFrancisco.id,
+        city: SanFrancisco.name,
+        cityId: SanFrancisco.id,
     },
     9: {
         name: 'One Bryant Park',
         bname: 'BoA Tower',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
     },
     10: {
         name: '56 Leonard',
         bname: '56 Leonard',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
     },
     11: {
         name: '601 Lexington',
         bname: 'Citigroup Center',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
     },
 
     // FOAM scavenger
     12: {
         name: '745 7th Ave',
         bname: '745 7th Ave',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
     },
 
     // token 1 - 5
@@ -94,8 +102,8 @@ module.exports = {
     1000001: {
         name: '375 Park Ave',
         bname: '375 Park Ave',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
     },
     1000002: {
         name: '133 Peachtree',
@@ -105,34 +113,34 @@ module.exports = {
     1000003: {
         name: '200 East Randolph',
         bname: '200 East Randolph',
-        city: cityConfig.SanFrancisco.name,
-        cityId: cityConfig.SanFrancisco.id,
+        city: SanFrancisco.name,
+        cityId: SanFrancisco.id,
     },
     1000004: {
         name: '600 Montgomery',
         bname: '600 Montgomery',
-        city: cityConfig.SanFrancisco.name,
-        cityId: cityConfig.SanFrancisco.id,
+        city: SanFrancisco.name,
+        cityId: SanFrancisco.id,
     },
 
     // NonFungible.com specials
     1000005: {
         name: '1 NonFungible Tower',
         bname: '1 NonFungible Tower',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id
+        city: NYC.name,
+        cityId: NYC.id
     },
     1000006: {
         name: '2 NonFungible Tower',
         bname: '2 NonFungible Tower',
-        city: cityConfig.SanFrancisco.name,
-        cityId: cityConfig.SanFrancisco.id,
+        city: SanFrancisco.name,
+        cityId: SanFrancisco.id,
     },
     1000007: {
         name: '3 NonFungible Tower',
         bname: '3 NonFungible Tower',
-        city: cityConfig.Chicago.name,
-        cityId: cityConfig.Chicago.id,
+        city: Chicago.name,
+        cityId: Chicago.id,
     },
 
     // Founder building
@@ -142,104 +150,104 @@ module.exports = {
     1000009: {
         name: 'Nakagin Capsule Tower',
         bname: 'Nakagin Capsule Tower',
-        city: cityConfig.Tokyo.name,
-        cityId: cityConfig.Tokyo.id,
+        city: Tokyo.name,
+        cityId: Tokyo.id,
         background_color: backgrounConfig.SPECIAL.hex,
         background_colorId: backgrounConfig.SPECIAL.id
     },
     1000010: {
         name: 'Balfron Tower',
         bname: 'Balfron Tower',
-        city: cityConfig.London.name,
-        cityId: cityConfig.London.id,
+        city: London.name,
+        cityId: London.id,
         background_color: backgrounConfig.PINK.hex,
         background_colorId: backgrounConfig.PINK.id
     },
     1000011: {
         name: '945 Madison Ave',
         bname: '945 Madison Ave',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.BEIGE.hex,
         background_colorId: backgrounConfig.BEIGE.id,
     },
     1000012: {
         name: '626 1st Ave',
         bname: '626 1st Ave',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.ORANGE.hex,
         background_colorId: backgrounConfig.ORANGE.id,
     },
     1000013: {
         name: '461 Dean Apartments',
         bname: '461 Dean Apartments',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.GRAY.hex,
         background_colorId: backgrounConfig.GRAY.id
     },
     1000014: {
         name: '481 8th Ave',
         bname: '481 Eighth Ave',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.PINK.hex,
         background_colorId: backgrounConfig.PINK.id
     },
     1000015: {
         name: '767 5th Ave',
         bname: '767 5th Ave',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.PINK.hex,
         background_colorId: backgrounConfig.PINK.id
     },
     1000016: {
         name: 'FOAM Tower',
         bname: 'FOAM Tower',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.LIGHT_ORANGE.hex,
         background_colorId: backgrounConfig.LIGHT_ORANGE.id
     },
     1000017: {
         name: '96 Wythe Ave',
         bname: '96 Wythe Ave',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.PINK.hex,
         background_colorId: backgrounConfig.PINK.id
     },
     1000018: {
         name: '215 Moore',
         bname: '215 Moore',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.SPECIAL.hex,
         background_colorId: backgrounConfig.SPECIAL.id
     },
     1000019: {
         name: '1 World Trade Center',
         bname: '1 World Trade Center',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.LIGHT_YELLOW.hex,
         background_colorId: backgrounConfig.LIGHT_YELLOW.id
     },
     1000020: {
         name: '96 Wythe Ave',
         bname: '96 Wythe Ave',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.LIGHT_ORANGE.hex,
         background_colorId: backgrounConfig.LIGHT_ORANGE.id
     },
     1000021: {
         name: '251 1st',
         bname: '251 1st',
-        city: cityConfig.NYC.name,
-        cityId: cityConfig.NYC.id,
+        city: NYC.name,
+        cityId: NYC.id,
         background_color: backgrounConfig.GRAY.hex,
         background_colorId: backgrounConfig.GRAY.id
     },
