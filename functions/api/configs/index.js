256cf8a77158723b42c5644b6bb97b3814f9ad55
Andy Gray <andy@whatsthescore.co.uk>
2019-07-24 11:03:22 +0100

    review comment

--- functions/api/configs/index.js
+++ functions/api/configs/index.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 const configs = require('express').Router({mergeParams: true});
 
 // Used for admin app - listing out specials we current can mint
-configs.get('/:building/base/:base/body/:body/roof/:roof/exterior/:exterior/windows/:windows', async (req, res, next) => {
+configs.get('/buildings/specials', async (req, res, next) => {
     response
         .status(200)
         .json(require('./services/metadata/special-data-mapping.js'));
