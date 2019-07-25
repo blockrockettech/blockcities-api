72125343e43a40be4174d2d4d05b6ea85ecda802
Andy Gray <andy@whatsthescore.co.uk>
2019-07-25 15:58:25 +0100

    trys

--- functions/api/configs/index.js
+++ functions/api/configs/index.js
@@ -4,9 +4,9 @@ const configs = require('express').Router({mergeParams: true});
 
 // Used for admin app - listing out specials we current can mint
 configs.get('/buildings/specials', async (req, res, next) => {
-    response
+    res
         .status(200)
-        .json(require('./services/metadata/special-data-mapping.js'));
+        .json(require('../../services/metadata/special-data-mapping.js'));
 });
 
 module.exports = configs;
