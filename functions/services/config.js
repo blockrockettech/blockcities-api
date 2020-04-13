cb3da19582b3ea5206b54892f9bc9bcf3e49b61d
Leonidas <web@pirron.one>
2020-04-13 19:38:03 +0200

    brooklyn collection

--- functions/services/config.js
+++ functions/services/config.js
@@ -7,12 +7,12 @@ const LIVE_SITE_ID = '5ca25aa53ef88d7811fa6a49';
 // const BUILD_PROFILES_COLLECTION_ID = '5cf917a298b9ff812bfc64d1';
 const BUILD_PROFILES_COLLECTION_ID = '5dcdad2aeb1a33420578f821';
 
-const webFlowApiKey = '1'; //functions.config().webflow.api.key;
+const webFlowApiKey = functions.config().webflow.api.key;
 if (!webFlowApiKey) {
     throw new Error('No WebFlow API Key found - is the project setup correctly - see README');
 }
 
-const openseaApiKey = '1'; //functions.config().opensea.api.key;
+const openseaApiKey = functions.config().opensea.api.key;
 if (!openseaApiKey) {
     throw new Error('No OpenSea API Key found - is the project setup correctly - see README');
 }
